fbce27c82e9786acfb0525b8dcc47df90f8416e9
Ali Abbas Chadhar <163995321+aliabbaschadhar@users.noreply.github.com>
2026-04-14 17:34:08 +0500

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-sudo docker pull aliabbaschadhar003/simple-python-flask-app:latest
+docker pull aliabbaschadhar003/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-sudo docker run -d -p 5000:5000 aliabbaschadhar003/simple-python-flask-app
+docker run -d -p 5000:5000 aliabbaschadhar003/simple-python-flask-app
