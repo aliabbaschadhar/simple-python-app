4d07f628bfc4b26cebcf5e3d0662d6db1036a882
Ali Abbas Chadhar <163995321+aliabbaschadhar@users.noreply.github.com>
2026-04-14 16:31:05 +0500

    Add sudo to Docker commands in start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull aliabbaschadhar003/simple-python-flask-app:latest
+sudo docker pull aliabbaschadhar003/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 aliabbaschadhar003/simple-python-flask-app
+sudo docker run -d -p 5000:5000 aliabbaschadhar003/simple-python-flask-app
